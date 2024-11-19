a4c7c24cbcfe710ea9096dc19a18defc3c83dfc9
Paul Niemczyk <mrpndev@gmail.com>
2024-11-18 19:12:16 -0500

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,26 @@ import ShowData from './components/ShowData/ShowData'
 
 function App() {
 
-  const [ count, setCount ] = useState(0)
-  const [ email, setEmail ] = useState("")
-  const [ pwd, setPwd ] = useState("")
+  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState("")
+  const [pwd, setPwd] = useState("")
+  const [showCount, setShowCount] = useState(false)
+
+  function handleField() {
+    if (showCount) {
+      return "Hide Count"
+    } else {
+      return "Show Count"
+    }
+  }
 
   return (
     <>
-      <Count count={count} setCount={setCount} />
+      <button onClick={() => setShowCount(!showCount)}>{handleField()}</button>
+      {showCount ? <Count count={count} setCount={setCount} /> : null}
       <Auth email={email} pwd={pwd} setEmail={setEmail} setPwd={setPwd} />
       <ShowData email={email} pwd={pwd} />
+      {count % 2 === 0 ? ": by zero" : "not : by 0"}
     </>
   )
 }
--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,4 +1,10 @@
+import { render } from "react-dom"
+import { useState } from "react"
+
 export default function Auth({ email, pwd, setEmail, setPwd }) {
+    const [register, setRegister] = useState(false)
+    const [fName, setFName] = useState("")
+    const [lName, setLName] = useState("")
 
     function handleClick(evt) {
         setEmail(evt.target.form[0].value)
@@ -6,11 +12,44 @@ export default function Auth({ email, pwd, setEmail, setPwd }) {
         console.log(email, pwd)
     }
 
+    const renderRegister = () => !register ? null : (
+        <>
+            <input onChange={e => setFName(e.target.value)}
+                type="text" value={fName}
+                placeholder="First Name" />
+            <input onChange={e => setLName(e.target.value)}
+                type="text"
+                value={lName}
+                placeholder="Last Name" />
+        </>
+    )
+
+
+    function handleRegisterBtn(e) {
+        e.preventDefault()
+        setRegister(!register)
+    }
+
+    const handleSwitchBtn = () => register ? "Login" : "Register"
+
+    /* 
+        ? Challenge
+        * Create a firstName and lastName input fields
+        * Create a button for register/login
+        * The default auth state should be for login
+        * If someone presses the register/login button, it should show fname, lname field
+        * The value of the button should now change from register back to login
+        * Ensure this works vice versa as well (register to login / login to register)
+        ! NOT SO SPICEY CHALLENGE (make it look pretty)
+    */
+
     return (
         <form>
+            {renderRegister()}
             <input onChange={e => console.log(e.target.value)} type="email" placeholder="Email" />
             <input type="password" name="" id="" placeholder="Password" />
             <input onClick={handleClick} type="button" value="Submit" />
+            <button onClick={handleRegisterBtn}>{handleSwitchBtn()}</button>
         </form>
     )
 }
